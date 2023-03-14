20d9e8da95ab6f7057dc0e21e4cc2f78f6554bf0
paulodallago <paulohdallago@gmail.com>
2023-03-14 15:12:55 -0300

    14.03.23

---
+++ script.js
@@ -0,0 +1,19 @@
+function showCards() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+function showHero() {
+    window.scrollTo(0, 0);
+}
+
+function showTimeline() {
+    window.open("timeline.html","_self");
+}
+
+function showIndex() {
+    window.open("index.html","_self");
+}
+
+function showAlbum() {
+    window.open("album.html","_self");
+}
